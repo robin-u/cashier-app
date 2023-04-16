4b0eb66c4754926b8bcd0a153f8bccaf84eb4eb3
Robin <robinundap@gmail.com>
2023-04-16 16:49:05 +0800

    Add new methods to update and delete from the cart

--- modules/features.py
+++ modules/features.py
@@ -1,7 +1,8 @@
+import tabulate
 class Transaction:
     # A list of dictionary to store item temporarily 
     cart_of_items = dict()
-
+    
     def __init__(self, customer_name, customer_id):
         self.customer_name = customer_name
         self.customer_id = customer_id
@@ -24,9 +25,8 @@ class Transaction:
         
         Returns:
             cart_of_items(dict): Update the current cart with new item(s)
-
         """
-        
+
         for item, quantity, price in zip(items, quantities, prices):
             if item in cart:
                 cart[item]['quantity'] += quantity
@@ -37,34 +37,128 @@ class Transaction:
         return self.cart_of_items
 
     # Customer updates the current item name to a new one
-    def update_item_name(current_name, new_name):
-        return 1
+    def update_item_name(self, current_name: str, new_name: str):
+        """
+        Update an item name that has been added to the cart
+        
+        Args:
+            current_name(str): An item name
+            new_name(str): New name for the item
+        
+        Returns:
+            None
+        """
+
+        try:
+            if current_name in self.cart_of_items:
+                if type(new_name) == str:
+                    self.cart_of_items[current_name] = self.cart_of_items[new_name]
+                else:
+                    raise TypeError()
+        except TypeError:
+            print("Data type is not a str")
+        except Exception as err:
+            raise Exception("An error occured, see details ->", err.args)
+        
 
     # Customer updates the current quantity of the item
-    def update_item_qty(current_qty, new_qty):
-        return 1
+    def update_item_qty(self, item_name: str, new_qty: int):
+        """
+        Update an item total quantity
+        
+        Args:
+            item_name(str): An item name  
+            new_qty(int): New quantity count for the item
+        
+        Returns:
+            None
+        """
+
+        try:
+            if item_name in self.cart_of_items:
+                if type(new_qty) == int:
+                    self.cart_of_items[item_name]['quantity'] = new_qty
+                else:
+                    raise TypeError()
+        except TypeError:
+            print("new_qty data type needs to be an integer")
+        except Exception as err:
+            raise Exception("An error occured, see details ->", err.args)
 
     # Customer updates the current price of the item
-    def update_item_price(current_price, new_price):
-        return 1
+    def update_item_price(self, item_name: str, new_price: int or float):
+        """
+        Update an item price
+        
+        Args:
+            item_name(str): An item name  
+            new_price(int or float): New price for the item
+        
+        Returns:
+            None
+        """
+
+        try:
+            if item_name in self.cart_of_items:
+                if type(new_price) == int or type(new_price) == float:
+                    self.cart_of_items[item_name]['price'] = new_price
+                else:
+                    raise TypeError()
+        except TypeError:
+            print("new_price data type needs to be an integer or a float")
+        except Exception as err:
+            raise Exception("An error occured, see details ->", err.args)
 
     # Customer deletes an item from the current transaction
-    def delete_item(item_name):
-        return 1
+    def delete_item(self, item_name: str):
+        """
+        Delete an item from the cart
+        
+        Args:
+            item_name(str): An item name  
+        
+        Returns:
+            None
+        """
+
+        try:
+            if item_name in self.cart_of_items:
+                self.cart_of_items.pop(item_name)
+        except Exception as err:
+            raise Exception("An error occured, see details ->", err.args)
 
     # Customer resets the whole transaction and starts from a new empty transaction
-    def reset_transaction():
-        return 1
+    def reset_transaction(self):
+        """
+        Delete the entire cart
+        
+        Args:
+            None
+        
+        Returns:
+            None
+        """
+
+        print("Are you sure to reset and start again the transaction?")
+        confirmation = input("type \"YES\" or \"NO\" (case sensitive)")
+
+        if confirmation == "YES":
+            self.cart_of_items.clear()
+        else:
+            print("Reset cart has been cancelled")
+            pass
 
     # Customer check their order
-    def check_order():
+    def check_order(self):
         """
         Check if customer order list (item name, qty, price and total) has been fully inputed.
         
-        Check if the total price per item and get discount:
-            1. Discount for 5% if total > Rp. 200.000
-            2. Discount for 6% if total > Rp. 300.000
-            3. Discount for 7% if total > Rp. 500.000
+        Checkthe total price per item and get discount, only if:
+            1. Total > Rp. 200.000 get discount for 5%
+            2. Total > Rp. 300.000 get discount for 6%
+            3. Total > Rp. 500.000 get discount for 7%
+        
+        Print the transaction in tabular form.
         
         Submit to database if the transaction is completed
         
@@ -72,7 +166,7 @@ class Transaction:
             None 
         
         Returns:
+            None
             str: "✅ Order has been completed" or "❌ There's something missing"
-            print: List of order(s) in tabular form
         """
-        return 1
+        
